bc5938c0fdec9ff9437e68dc3a717bb1a85cbf5a
Creationdis <Creationdis@gmail.com>
2023-04-17 10:22:53 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - name of the file
+ * it was compiled from
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+return (0);
+}
