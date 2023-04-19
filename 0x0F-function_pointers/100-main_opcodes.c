f93fd6febe89d29a74711eea2eb5f5e82455b671
Creationdis <Creationdis@gmail.com>
2023-04-19 09:10:02 -0700

    task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,36 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+**/
+void print_opcodes(char *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%.2hhx", a[i]);
+if (i < n - 1)
+printf(" ");
+}
+printf("\n");
+
+}
 
 /**
  * main - prints the opcodes of its own main function
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0
+ * Return: always O
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int n, bytes;
-char *main_ptr;
+int n;
 
 if (argc != 2)
 {
 printf("Error\n");
-return (1);
+exit(1);
 }
-
-bytes = atoi(argv[1]);
-if (bytes < 0)
+n = atoi(argv[1]);
+if (n < 0)
 {
 printf("Error\n");
-return (2);
+exit(2);
 }
-
-main_ptr = (char *) main;
-for (n = 0; n < bytes; n++)
-printf("%02x ", main_ptr[n] & 0xff);
-printf("\n");
-
+print_opcodes((char *)&main, n);
 return (0);
 }
-
