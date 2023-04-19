6c59aa801e4910f61faf6a48bfc51f8ef6264a88
Creationdis <Creationdis@gmail.com>
2023-04-19 08:54:01 -0700

    task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,23 +10,25 @@
  */
 int main(int argc, char **argv)
 {
-	int i, bytes;
-	char *main_ptr;
+int n, bytes;
+char *main_ptr;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	i = atoi(argv[1]);
-	if (i < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	main_ptr = (char *) main;
-for (i = 0; i < bytes; i++)
-printf("%02x ", main_ptr[i] & 0xff);
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
+
+main_ptr = (char *) main;
+for (n = 0; i < bytes; n++)
+printf("%02x ", main_ptr[n] & 0xff);
 printf("\n");
 
 return (0);
