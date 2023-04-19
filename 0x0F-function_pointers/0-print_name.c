c261388c14cc5fde1695cf6684c42aaad03efd5d
Creationdis <Creationdis@gmail.com>
2023-04-19 06:58:55 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include"stdlib.h"
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - a function that  prints a name
+ * @name: the name
+ * @f: the function of the name
+ * Return: 0
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
+}
