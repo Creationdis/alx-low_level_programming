590fca61829dc2fabd77c8805cc6afa53f0950cc
creationdis <creationdis@gmail.com>
2023-04-27 05:28:37 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,13 +7,17 @@
  *
  * Return: The number of nodes in the list
  */
+ 
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
 
-while (h != NULL)
+while (h)
 {
-printf("[%d] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 count++;
 }
