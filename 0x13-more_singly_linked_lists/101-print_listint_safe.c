0be20ea2401811a5babb3aeaa040655f9b1c1f5d
creationdis <creationdis@gmail.com>
2023-04-29 08:44:00 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,21 +10,20 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *current = head;
-    size_t count = 0;
+const listint_t *current = head;
+size_t count = 0;
 
-    while (current != NULL)
-    {
-        printf("[%p] %d\n", (void *)current, current->n);
-        count++;
-        if (current <= current->next)
-        {
-            printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-            exit(98);
-        }
-        current = current->next;
-    }
-
-    return (count);
+while (current != NULL)
+{
+printf("[%p] %d\n", (void *)current, current->n);
+count++;
+if (current <= current->next)
+{
+printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+exit(98);
+}
+current = current->next;
+}
+return (count);
 }
 
