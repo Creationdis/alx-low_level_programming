d2ab52b2e8671a62b237749a296f8e54b4bf0da4
creationdis <creationdis@gmail.com>
2023-05-01 04:56:00 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,43 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely
- * @head: Pointer to the first node of the list
+ * print_listint_safe - prints a listint_t linked list safely
+ * @head: pointer to the first node of the list
  *
- * Return: The number of nodes in the list
+ * Return: number of nodes in the list
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i = 0;
-	long int count;
+size_t i = 0;
+const listint_t *temp, *arr[1024];
 
-	while (head)
-	{
-		count = head - head->next;
-		i++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (count > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
-	}
+if (head == NULL)
+exit(98);
 
-	return (i);
+temp = head;
+while (temp != NULL && i < 1024)
+{
+printf("[%p] %d\n", (void *)temp, temp->n);
+
+arr[i] = temp;
+i++;
+
+temp = temp->next;
+if (temp == head)
+{
+printf("-> [%p] %d\n", (void *)temp, temp->n);
+break;
+}
+}
+
+if (i >= 1024)
+{
+fprintf(stderr, "Error: list is too long\n");
+exit(98);
+}
+
+return (i);
 }
 
