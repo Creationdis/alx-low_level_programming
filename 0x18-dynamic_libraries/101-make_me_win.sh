0ad4e143218678775bd065eafefc0833802ad91c
Creationdis <Creationdis@gmail.com>
2023-06-19 10:53:57 -0700

    lkdsjfgmdlkfjg

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp -q "https://github.com/Creationdis/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmmw.so"
-export LD_PRELOAD="/tmp/libmmw.so"
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
