26111697fcf2a6a9fd20fb191f76eda644197594
Creationdis <Creationdis@gmail.com>
2023-06-20 16:11:22 -0700

    kjgmflkjg

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/lib_inject.so https://github.com/Elmahdi1962/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib_inject.so?raw=true
-export LD_PRELOAD=$HOME/lib_inject.so
+wget -O /tmp/test.so https://github.com/Creationdis/alx-low_level_programming/raw/master/0x18-dynamic_libraries/test.so
+export LD_PRELOAD=/tmp/test.so
